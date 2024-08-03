08d866a04e0c1c81b1e7bb0e7db832413092a663
IpvLeo <leoanarcopunk77@gmail.com>
2024-08-03 15:57:31 -0300

    apredizados das aulas

--- src/typevariables/TiposVariaveis.java
+++ src/typevariables/TiposVariaveis.java
@@ -1,5 +1,21 @@
 package typevariables;
 
 public class TiposVariaveis {
+	public static void main(String[] args) {
+		short numeroCurto = 1;
+		int numeroNormal = numeroCurto;
+		short numerocurto2 = (short) numeroNormal;
 
+		int numero = 1;
+
+		numero = 5;
+		numero = 10;
+		
+		System.out.println(numero);
+		
+		final double VALOR_DE_PI = 3.14;
+		VALOR_DE_PI =11;
+		//final não pode ser modificiada
+		
+	}
 }
